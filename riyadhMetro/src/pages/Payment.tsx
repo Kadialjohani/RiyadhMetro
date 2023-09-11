7f41d1e4541bb29a5706996d0f7b8e11ca0c102a
ll_admin <kadialjohani2002@gmail.com>
2023-09-11 23:49:48 +0300

    unPayment

--- riyadhMetro/src/pages/Payment.tsx
+++ riyadhMetro/src/pages/Payment.tsx
@@ -1,27 +1,27 @@
-import { CardElement, Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
+// import { CardElement, Elements } from "@stripe/react-stripe-js";
+// import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromise = loadStripe(
-  "pk_test_51NpA9ABjKC9tyQ3OgsLeWsL158hTYR1HoUiictaRKpZ5pmUyX0dHgXjlyY7qNjhODmJALNjpZooX65ht341fgwH700oFp2426t"
-);
+// const stripePromise = loadStripe(
+//   "pk_test_51NpA9ABjKC9tyQ3OgsLeWsL158hTYR1HoUiictaRKpZ5pmUyX0dHgXjlyY7qNjhODmJALNjpZooX65ht341fgwH700oFp2426t"
+// );
 
-function showAlert() {
-  alert("This is an alert message!");
-}
+// function showAlert() {
+//   alert("This is an alert message!");
+// }
 
-export default function Payment() {
-  return (
-    <div className="payment-container">
-      <h1>Payment Form</h1>
-      <Elements stripe={stripePromise}>
-        <form id="pay" className="payment-form">
-          <label htmlFor="card-element">Card Details</label>
-          <CardElement className="card-element" />
-          <button onClick={showAlert} type="submit" className="pay-button">
-            Pay
-          </button>
-        </form>
-      </Elements>
-    </div>
-  );
-}
+// export default function Payment() {
+//   return (
+//     <div className="payment-container">
+//       <h1>Payment Form</h1>
+//       <Elements stripe={stripePromise}>
+//         <form id="pay" className="payment-form">
+//           <label htmlFor="card-element">Card Details</label>
+//           <CardElement className="card-element" />
+//           <button onClick={showAlert} type="submit" className="pay-button">
+//             Pay
+//           </button>
+//         </form>
+//       </Elements>
+//     </div>
+//   );
+// }
