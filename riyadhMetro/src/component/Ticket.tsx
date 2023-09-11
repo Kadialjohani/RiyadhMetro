f148a88da26275cd538b5e247f05abdb90870f68
ll_admin <kadialjohani2002@gmail.com>
2023-09-11 18:11:02 +0300

    manageBookings Redesign

--- riyadhMetro/src/component/Ticket.tsx
+++ riyadhMetro/src/component/Ticket.tsx
@@ -16,23 +16,23 @@ export default function Ticket(props:details) {
   return (
     
       <div className='flex justify-center items-center h-3/4 w-full my-10 relative'>
-                <img className='' src={TicketForm}></img>
-                <div className='absolute top-10 left-12'>
-                  <h1 className='mb-2 text-xl font-bold text-[#176B87]'>From</h1>
+                <img className='w-full' src={TicketForm}></img>
+                <div className='absolute top-16 left-20'>
+                  <h1 className='mb-2 text-3xl font-bold text-[#176B87]'>From</h1>
                   <h1 className='text-lg font-bold text-[#64CCC5] w-4/6'>{props.from}</h1>
                 </div>
-                <div className='absolute top-10 left-80'>
-                  <h1 className='mb-2 text-xl font-bold text-[#176B87]'>To</h1>
+                <div className='absolute top-16 left-96'>
+                  <h1 className='mb-2 text-3xl font-bold text-[#176B87]'>To</h1>
                   <h1 className='text-lg font-bold text-[#64CCC5] w-4/6'>{props.to}</h1>
                 </div>
                 <div className='absolute top-48 left-52'>
-                  <h1 className='mb-2 text-xl font-bold text-[#176B87]'>Date</h1>
+                  <h1 className='mb-2 text-2xl font-bold text-[#176B87]'>Date</h1>
                   <div className='flex flex-row items-center'>
                   <FontAwesomeIcon className='text-[#176B87]' icon={faCalendarDays}/>
                   <h1 className='text-xl font-bold text-[#64CCC5] pl-2'>{props.date}</h1>
                   </div>
                 </div>
-                <div className='absolute flex flex-col h-36 justify-between top-8 right-14'>
+                <div className='absolute flex flex-col h-36 justify-between top-10 right-24'>
                   <div className='flex flex-row justify-between w-52'>
                   <button className='mb-2 text-2xl text-[#EEEEEE] font-bold bg-[#176B87] w-20 h-10 rounded-full'><FontAwesomeIcon icon={faPenToSquare} /></button>
                   <button className='mb-2 text-2xl text-[#EEEEEE] font-bold bg-[#871717] w-20 h-10 rounded-full'><FontAwesomeIcon icon={faTrash} /></button>
