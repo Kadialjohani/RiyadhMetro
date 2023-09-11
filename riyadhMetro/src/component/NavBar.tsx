21c70b75c59dc66b585d74b64f2d11887ac6f50c
ll_admin <kadialjohani2002@gmail.com>
2023-09-11 16:12:36 +0300

    redesign1xx

--- riyadhMetro/src/component/NavBar.tsx
+++ riyadhMetro/src/component/NavBar.tsx
@@ -11,10 +11,12 @@ interface NavbarProps {
 
   const Navbar: React.FC<NavbarProps> = ({ isLoggedIn, onLogin, onSignup, onLogout }) => {
     return (
-      <nav className='h-80px absolute w-full z-20'>
-        <ul className='flex flex-row justify-between mx-24 font-bold text-lg text-[#EEEEEE]'>
+      <>
+      
         {!isLoggedIn && (
             <>
+            <nav className='h-80px absolute w-full z-20'>
+        <ul className='flex flex-row justify-between mx-24 font-bold text-lg text-[#EEEEEE]'>
             <div className='flex flex-row justify-between items-center w-3/5'>
           <li>
             <a href="/"><img className='w-36' src={Logo}></img></a>
@@ -37,10 +39,14 @@ interface NavbarProps {
                 <button className='bg-[#176B87] w-24 h-10 rounded-full' onClick={onSignup}>Signup</button>
               </li>
               </div>
+              </ul>
+              </nav>
             </>
           )}
           {isLoggedIn && (
             <>
+            <nav className='h-80px w-full bg-slate-400'>
+        <ul className='flex flex-row justify-between mx-24 font-bold text-lg text-[#EEEEEE]'>
             <div className='flex flex-row justify-between items-center w-3/6'>
             <li>
               <a href="/home"><img className='w-36' src={Logo}></img></a>
@@ -57,11 +63,13 @@ interface NavbarProps {
               <button className='bg-[#176B87] w-24 h-10 rounded-full' onClick={onLogout}>Logout</button>
             </li>
             </div>
+            </ul>
+      </nav>
             </>
           )}
         
-        </ul>
-      </nav>
+        
+      </>
     );
   };
   
