1bfc7a1d5927611390035b3995b0ca7c21d6fe49
Majd <majdthani@gmail.com>
2023-09-12 21:21:25 +0300

    edit of login page responsive

--- riyadhMetro/src/pages/LogInPage.tsx
+++ riyadhMetro/src/pages/LogInPage.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import img from "../assets/loginImg.png";
 import Logo from "../assets/Logo.png";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 
 type Info = {
   email: string;
@@ -27,58 +27,72 @@ export default function LogInPage() {
     });
     localStorage.setItem("isLog", IsLogin);
 
-    if (IsLogin){
-      nav("/home")
+    if (IsLogin) {
+      nav("/home");
     } else {
-      
       Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Email or Password are incorrect',
-        
-      })
-      
-    }}
-  
+        icon: "error",
+        title: "Oops...",
+        text: "Email or Password are incorrect",
+      });
+    }
+  };
 
   return (
     <>
-    <a href="/"><img className="w-36 absolute pl-2" src={Logo}></img></a>
-    <div className="flex flex-row justify-between items-center w-full h-screen">
-      <div className="flex flex-col items-center shadow-2xl shadow-[#176B87] border-2 border-[#176B87] w-1/2 h-3/5 ml-40 rounded-3xl">
-        <h1 className="font-bold text-6xl my-8  text-[#176B87]">Log In</h1>
-      
-        <div className="flex justify-start flex-col w-4/5">
-          <div className="flex flex-col mb-10 w-full">
-            <label className="text-xl font-bold  text-[#176B87]">Email</label>
-            <input
-              className="rounded-md h-10 border-2 border-[#176B87]"
-              type="text"
-              value={email}
-              onChange={(e) => setEmail(e.target.value )}
-            ></input>
+      <a href="/">
+        <img className="w-36 absolute pl-2" src={Logo}></img>
+      </a>
+      <div className="flex flex-row  justify-center lg:justify-end items-center xl:justify-end w-full h-screen ">
+        <div className="flex flex-col items-center shadow-2xl shadow-[#176B87] border-2 m-6 md:m-14 border-[#176B87] lg:w-[40%]  h-[66%] md:h-[60%] lg:h-[66%] w-[100%] rounded-3xl">
+          <h1 className="font-bold p-3 lg:p-10 text-4xl md:text-5xl lg:text-6xl my-8 text-[#176B87]">
+            Log In
+          </h1>
+          <div className="flex justify-start flex-col w-4/5 ">
+            <div className="flex flex-col mb-10 w-full">
+              <label className=" text-xl md:text-2xl lg:text-2xl font-bold text-[#176B87] p-3">
+                Email:
+              </label>
+              <input
+                className="rounded-md h-10 md:h-14 lg:h-14 w-4/4 border-2 border-[#176B87]"
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+            </div>
+            <div className="flex flex-col w-full ">
+              <label className="text-xl md:text-2xl lg:text-2xl font-bold  text-[#176B87] p-3 ">
+                Password:
+              </label>
+              <input
+                className="rounded-md h-10 md:h-14 lg:h-14 w-4/4 border-2 border-[#176B87]"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+            </div>
           </div>
-          <div className="flex flex-col w-full">
-            <label className="text-xl font-bold  text-[#176B87]">Password</label>
-            <input
-              className="rounded-md h-10 border-2 border-[#176B87]"
-              type="password"
-              value={password}
-              onChange={(e) =>
-                setPassword(e.target.value)
-              }
-            ></input>
-          </div>
+          <button
+            onClick={handleLogIn}
+            className="rounded-xl w-[40%] h-[10%] bg-[#176B87] p-3 mt-[50px] md:mt-20 lg:mt-20 text-[#EEEEEE] font-bold text-xl md:text-3xl lg:text-3xl "
+          >
+            Log In
+          </button>
+          <button
+            type="submit"
+            className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          >
+            Sign in
+          </button>
+          <p className="lg:text-xl md:text-xl text-l font-light text-[#176B87]">
+            Don’t have an account yet?
+            <a href="signup" className="text-[#176B87] hover:underline">
+              Sign up
+            </a>
+          </p>
         </div>
-        <button
-          onClick={handleLogIn}
-          className="rounded-xl w-72 h-12 bg-[#176B87] mt-12 text-[#EEEEEE] font-bold text-2xl"
-        >
-          Log In
-        </button>
+        <img className="hidden lg:block h-screen w-1/2" src={img}></img>
       </div>
-      <img className="h-screen w-1/2" src={img}></img>
-    </div>
     </>
   );
 }
--- riyadhMetro/src/pages/Payment.tsx
+++ riyadhMetro/src/pages/Payment.tsx
@@ -1,53 +1,43 @@
-
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Ticket from "../component/Ticket";
-import Swal from 'sweetalert2'
-
-
-
-
+import Swal from "sweetalert2";
 
 export default function Payment() {
-  const nav = useNavigate()
+  const nav = useNavigate();
   // function showAlert() {
   //   alert();
   //   nav("/bookings")
-    // post code
-    const showAlert = () => {
-      axios
-        .post("https://64fc603b605a026163ae6c99.mockapi.io/tickets", {
-          from: localStorage.getItem("from"),
-          to: localStorage.getItem("to"),
-          date: localStorage.getItem("date"),
-          price: localStorage.getItem("price"),
-        })
-        .then((res) => 
-          // setList([...list, res.data]);
-          // setDate(date)
-          console.log(res));
-          
-          
-       
-        
-    };
-  
+  // post code
+  const showAlert = () => {
+    axios
+      .post("https://64fc603b605a026163ae6c99.mockapi.io/tickets", {
+        from: localStorage.getItem("from"),
+        to: localStorage.getItem("to"),
+        date: localStorage.getItem("date"),
+        price: localStorage.getItem("price"),
+      })
+      .then((res) =>
+        // setList([...list, res.data]);
+        // setDate(date)
+        console.log(res)
+      );
+  };
 
   return (
-
-    
-
     <div className="bg-[#176B87] h-screen flex flex-col justify-center items-center">
-    <div className="rounded-xl bg-white w-3/4 flex flex-col p-5">
-      <Ticket from={localStorage.getItem("from")} to={localStorage.getItem("to")} date={localStorage.getItem("date")} price={localStorage.getItem("price").toString()}></Ticket>
-    {/* {localStorage.getItem("from")} */}
-    <div>
-
-      <a href="https://buy.stripe.com/test_5kA3exdvr7mN5pK6oo">Payment</a>
-
+      <div className="rounded-xl bg-white w-3/4 flex flex-col p-5">
+        <Ticket
+          from={localStorage.getItem("from")}
+          to={localStorage.getItem("to")}
+          date={localStorage.getItem("date")}
+          price={localStorage.getItem("price").toString()}
+        ></Ticket>
+        {/* {localStorage.getItem("from")} */}
+        <div>
+          <a href="https://buy.stripe.com/test_5kA3exdvr7mN5pK6oo">Payment</a>
+        </div>
+      </div>
     </div>
   );
 }
-
-
-
