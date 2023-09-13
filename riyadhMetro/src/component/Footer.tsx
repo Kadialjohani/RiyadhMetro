a74d915424c58729d7285ee5994c6db8003886ee
Majd <majdthani@gmail.com>
2023-09-13 14:02:29 +0300

    edit of responsive

--- riyadhMetro/src/component/Footer.tsx
+++ riyadhMetro/src/component/Footer.tsx
@@ -1,92 +1,94 @@
-import React from 'react'
-import Logo from '../assets/Logo.png'
-import Logo2 from "../assets/Logo2.png"
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSquareInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faSquareXTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-
+import React from "react";
+import Logo2 from "../assets/Logo2.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faSquareXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 interface FooterProps {
   isLoggedIn: boolean;
-
 }
 
-const Footer: React.FC<FooterProps> = ({ isLoggedIn }) =>  {
+const Footer: React.FC<FooterProps> = ({ isLoggedIn }) => {
   return (
-    <div className='bg-[#053B50] w-full h-40 flex'>
-  
-        <ul className='flex flex-row justify-between items-center w-full mx-24 font-bold text-lg text-[#EEEEEE]'>
+    <div className="bg-[#053B50] w-full lg:h-40  md:h-[60px] h-[60px] flex justify-center">
+      <ul className="flex flex-row lg:justify-between justify-center lg:items-center items-center lg:w-full lg:mx-24 font-bold lg:text-lg text-[#EEEEEE]">
         {!isLoggedIn && (
-            <>
-            <div className='flex flex-row  items-center justify-between text-6xl w-1/6'>
-          
-          <li>
-          <a href='https://www.instagram.com/riyadhtransport/'><FontAwesomeIcon icon={faSquareInstagram} /></a>
-          </li>
-          <li>
-            <a href="https://twitter.com/RiyadhTransport"><FontAwesomeIcon icon={faSquareXTwitter} /></a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/company/ministry-of-transport-saudi-arabia/"><FontAwesomeIcon icon={faLinkedin} /></a>
-          </li>
-          </div>
-          <div className='flex flex-row justify-between items-center w-3/5'>
-          <li>
-            <a href="#services" >Services</a>
-          </li>
-          
-          <li>
-            <a href="#stations">Stations</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          
+          <>
+            <div className="flex flex-row lg:items-center lg:justify-between items-center justify-center lg:text-6xl lg:w-1/6">
               <li>
-              <a href="/"><img className='w-36' src={Logo2}></img></a>
+                <a href="https://www.instagram.com/riyadhtransport/">
+                  <FontAwesomeIcon icon={faSquareInstagram} />
+                </a>
               </li>
-              
-              </div>
-            </>
-          )}
-          {isLoggedIn && (
-            <>
-              <div className='flex flex-row  items-center justify-between text-6xl w-1/6'>
-          
-          <li>
-          <a href='https://www.instagram.com/riyadhtransport/'><FontAwesomeIcon icon={faSquareInstagram} /></a>
-          </li>
-          <li>
-            <a href="https://twitter.com/RiyadhTransport"><FontAwesomeIcon icon={faSquareXTwitter} /></a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/company/ministry-of-transport-saudi-arabia/"><FontAwesomeIcon icon={faLinkedin} /></a>
-          </li>
-          </div>
-          <div className='flex flex-row justify-between items-center w-3/5'>
-          
-          <li>
-              <a href="/bookings">Manage Bookings</a>
-            </li>
-            <li>
-              <a href="/home">Home</a>
-            </li>
               <li>
-              <a href="/home"><img className='w-36' src={Logo2}></img></a>
+                <a href="https://twitter.com/RiyadhTransport">
+                  <FontAwesomeIcon icon={faSquareXTwitter} />
+                </a>
               </li>
-              
-              </div>
-            </>
-          )}
-        
-        </ul>
-      
+              <li>
+                <a href="https://www.linkedin.com/company/ministry-of-transport-saudi-arabia/">
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+              </li>
+            </div>
+            <div className="flex flex-row lg:justify-between lg:items-center it lg:w-3/5">
+              <li>
+                <a href="#services">Services</a>
+              </li>
+
+              <li>
+                <a href="#stations">Stations</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+
+              <li>
+                <a href="/">
+                  <img className="lg:w-36 w-32" src={Logo2}></img>
+                </a>
+              </li>
+            </div>
+          </>
+        )}
+        {isLoggedIn && (
+          <>
+            <div className="flex flex-row lg:items-center gap-2 lg:justify-between items-center j lg:text-6xl lg:w-1/6 ">
+              <li>
+                <a href="https://www.instagram.com/riyadhtransport/">
+                  <FontAwesomeIcon icon={faSquareInstagram} />
+                </a>
+              </li>
+              <li>
+                <a href="https://twitter.com/RiyadhTransport">
+                  <FontAwesomeIcon icon={faSquareXTwitter} />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/company/ministry-of-transport-saudi-arabia/">
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+              </li>
+            </div>
+            <div className="flex flex-row gap-2 mt-2 ml-5 lg:justify-between lg:items-center lg:w-3/5">
+              <li>
+                <a href="/bookings">Manage Bookings</a>
+              </li>
+              <li>
+                <a href="/home">Home</a>
+              </li>
+              <li>
+                <a href="/home">
+                  <img className="lg:w-36 w-20" src={Logo2}></img>
+                </a>
+              </li>
+            </div>
+          </>
+        )}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
-
-
--- riyadhMetro/src/component/NavBar.tsx
+++ riyadhMetro/src/component/NavBar.tsx
@@ -1,6 +1,4 @@
-import React from "react";
-import Logo from "../assets/Logo.png";
-import Logo2 from "../assets/Logo2.png"
+import Logo2 from "../assets/Logo2.png";
 
 interface NavbarProps {
   isLoggedIn: boolean;
@@ -17,89 +15,86 @@ const Navbar: React.FC<NavbarProps> = ({
 }) => {
   return (
     <>
-    
-    
-        {!isLoggedIn && (
-          <>
-          <nav className="h-80px absolute w-full z-20">
-      <ul className="flex flex-row justify-between mx-24 font-bold text-xl text-[#EEEEEE]">
-            <div className="flex flex-row justify-between items-center w-3/5">
-              <li>
-                <a href="/">
-                  <img className="w-36" src={Logo2}></img>
-                </a>
-              </li>
-              <li >
-                <a  href="#about">About</a>
-              </li>
-              <li>
-                <a href="#stations">Stations</a>
-              </li>
-              <li>
-                <a href="#services" className="">
-                  Services
-                </a>
-              </li>
-            </div>
-            <div className="flex flex-row items-center justify-around w-1/5">
-              <li>
-                <a href="/login">
-                  <button
-                    className="border-2 border-[#EEEEEE] text-[#EEEEEE] w-24 h-10 rounded-full"
-                    onClick={onLogin}
-                  >
-                    Login
-                  </button>
-                </a>
-              </li>
-              <li>
-                <a href="/signup">
-                  <button
-                    className="bg-[#EEEEEE] text-[#176B87] w-24 h-10 rounded-full"
-                    onClick={onSignup}
-                  >
-                    Signup
-                  </button>
-                </a>
-              </li>
-            </div>
+      {!isLoggedIn && (
+        <>
+          <nav className="h-80px absolute w-full z-20 mt-4 ">
+            <ul className="flex flex-row justify-evenly md:gap-14 md:justify-start lg:flex-row lg:justify-between mx-24 font-bold text-l  md:text-xl lg:text-xl text-[#EEEEEE]">
+              <div className="flex flex-row justify-end gap-3 lg:flex-row lg:justify-between items-center w-3/5">
+                <li>
+                  <a href="/">
+                    <img className="w-36" src={Logo2}></img>
+                  </a>
+                </li>
+                <li>
+                  <a href="#about">About</a>
+                </li>
+                <li>
+                  <a href="#stations">Stations</a>
+                </li>
+                <li>
+                  <a href="#services" className="">
+                    Services
+                  </a>
+                </li>
+              </div>
+              <div className="flex flex-row gap-[0.6px] md:gap-5 lg:gap-1 justify-start md:justify-start items-center lg:flex-row lg:items-center lg:justify-around w-1/5">
+                <li>
+                  <a href="/login">
+                    <button
+                      className="border-2 border-[#EEEEEE] text-[#EEEEEE] w-[4pc] h-8 md:w-24 md:h-10 lg:w-24 lg:h-10 rounded-full"
+                      onClick={onLogin}
+                    >
+                      Login
+                    </button>
+                  </a>
+                </li>
+                <li>
+                  <a href="/signup">
+                    <button
+                      className="bg-[#EEEEEE] text-[#176B87] w-[4pc] h-8  md:w-24 md:h-10 lg:w-24 lg:h-10 rounded-full"
+                      onClick={onSignup}
+                    >
+                      Signup
+                    </button>
+                  </a>
+                </li>
+              </div>
             </ul>
-            </nav>
-          </>
-        )}
-        {isLoggedIn && (
-          <>
-          <nav className="h-80px bg-gradient-to-b from-[#64CCC5] to-[#053B50] w-full">
-      <ul className="flex flex-row justify-between mx-24 font-bold text-xl text-[#EEEEEE]">
-            <div className="flex flex-row justify-between items-center w-3/6">
-              <li>
-                <a href="/home">
-                  <img className="w-36" src={Logo2}></img>
-                </a>
-              </li>
-              <li>
-                <a href="/home">Home</a>
-              </li>
-              
-              <li>
-                <a href="/bookings">Manage Bookings</a>
-              </li>
-            </div>
-            <div className="flex flex-row items-center justify-around w-1/5">
-              <li>
-                <button
-                  className="bg-[#EEEEEE] text-[#176B87] w-24 h-10 rounded-full"
-                  onClick={onLogout}
-                >
-                  Logout
-                </button>
-              </li>
-            </div>
+          </nav>
+        </>
+      )}
+      {isLoggedIn && (
+        <>
+          <nav className="lg:h-[80px] md:h-[80px] h-[60px] bg-gradient-to-b from-[#64CCC5] to-[#053B50] w-full">
+            <ul className="flex flex-row lg:justify-between  justify-centermd:justify-center gap-5 font-bold text-sm text-center md:text-xl lg:text-2xl text-[#EEEEEE]">
+              <div className="flex flex-row lg:justify-between mt-3 md:mt-2 justify-end md:justify-between gap-3 items-center w-3/6">
+                <li>
+                  <a href="/home">
+                    <img className="w-36" src={Logo2}></img>
+                  </a>
+                </li>
+                <li>
+                  <a href="/home">Home</a>
+                </li>
+
+                <li>
+                  <a href="/bookings">Manage Bookings</a>
+                </li>
+              </div>
+              <div className="flex flex-row items-center mt-3 md:justify-end lg:justify-around w-1/5">
+                <li>
+                  <button
+                    className="bg-[#EEEEEE] text-[#176B87] w-20 h-9 lg:w-28 lg:h-11 md:w-28 md:h-11 rounded-full"
+                    onClick={onLogout}
+                  >
+                    Logout
+                  </button>
+                </li>
+              </div>
             </ul>
-    </nav>
-          </>
-        )}
-      
+          </nav>
+        </>
+      )}
     </>
   );
 };
