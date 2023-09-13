348319458563d013f1662808f9dda15cc1ec413d
Majd <majdthani@gmail.com>
2023-09-13 10:32:51 +0300

    navbar ,landing , login ,sing up  page responsive

--- riyadhMetro/src/component/Card.tsx
+++ riyadhMetro/src/component/Card.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-
 interface card {
-  src: string,
-  title: string,
+  src: string;
+  title: string;
 }
-export default function Card(props:card) {
+export default function Card(props: card) {
   return (
-    <div className='lg:w-2/6 md:w-full md:h-1/5 sm:w-full sm:h-1/5 lg:h-2/5  rounded-3xl bg-[#EEEEEE] hover:translate-y-5'>
-      <img src={props.src} className='h-3/4 w-full rounded-t-3xl'></img>
-      <h1 className='h-1/4 font-bold flex justify-center items-center text-xl text-[#053B50]'>{props.title}</h1>
-    </div>
-  )
+    <>
+      <div className=" lg:w-2/6 md:w-full md:h-1/5 lg:h-2/5 m-6 rounded-3xl bg-[#EEEEEE] hover:translate-y-5">
+        <img
+          src={props.src}
+          className="lg:h-3/4 w-full h-3/4 rounded-t-3xl"
+        ></img>
+        <h1 className="h-1/4 font-bold flex justify-center items-center lg:text-4xl text-l text-[#053B50]">
+          {props.title}
+        </h1>
+      </div>
+    </>
+  );
 }
--- riyadhMetro/src/pages/LandingPage.tsx
+++ riyadhMetro/src/pages/LandingPage.tsx
@@ -1,12 +1,9 @@
-import React from "react";
 import { useState } from "react";
 import NavBar from "../component/NavBar";
 import Footer from "../component/Footer";
-
-
 import Card from "../component/Card";
 import vidSrc from "../assets/vid2.mp4";
-import bgimg from "../assets/bgmdsm.jpg"
+import bgimg from "../assets/bgmdsm.jpg";
 import icon1 from "../assets/ic1.png";
 import icon2 from "../assets/ic2.png";
 import icon3 from "../assets/ic3.png";
@@ -42,54 +39,74 @@ export default function LandingPage() {
           onSignup={handleSignup}
           onLogout={handleLogout}
         />
-        <video className="z-0 w-full absolute md:hidden sm:hidden lg:inline" autoPlay loop muted>
+        <video
+          className="z-0 w-full absolute  hidden lg:block"
+          autoPlay
+          loop
+          muted
+        >
           <source src={vidSrc} type="video/mp4" />
         </video>
-        <img className="z-0 sm:h-96 h-[88.5vh] absolute lg:hidden" src={bgimg}></img>
-        <div className="z-10 w-full lg:h-[89.5vh] sm:h-96 bg-gradient-to-b from-[#64CCC5] to-[#053B50] opacity-75 absolute"></div>
-        <div className="z-20 lg:w-3/5 sm:w-80 lg:h-80 sm:h-40 flex flex-col justify-center items-center border-2 border-[#EEEEEE] rounded-3xl md:top-96 sm:top-56 top-96 left-1/2 transform -translate-x-1/2 -translate-y-1/2 absolute">
-          <h1 className="font-bold lg:text-6xl md:text-3xl sm:text-lg lg:mb-14 md:mb-14 sm:mb-6 text-[#EEEEEE]">
+        <img className="z-0 h-80  absolute lg:hidden" src={bgimg}></img>
+
+        <div className="z-10 w-full lg:h-[89.5vh] h-80 md:h-[69%] bg-gradient-to-b from-[#64CCC5] to-[#053B50] opacity-75 absolute"></div>
+        <div className="z-20 h-32 w-[70%] md:h-48 md:w-3/5 lg:h-80 lg:w-3/5 p-3 lg:top-96 top-36 flex flex-col justify-center items-center border-2 border-[#EEEEEE] rounded-3xl md:top-60 left-1/2 transform -translate-x-1/2 -translate-y-1/2 absolute">
+          <h1 className="font-bold lg:text-6xl md:text-3xl text-lg text-center lg:mb-14 md:mb-14 mb-6 text-[#EEEEEE]">
             We Transport Anywhere
           </h1>
           <a href="/login">
-            <button className="rounded-3xl lg:w-72 sm:w-40 h-12 font-bold lg:text-xl sm:text-lg bg-[#64CCC5] text-[#053B50] ">
+            <button className="rounded-3xl lg:w-72 lg:h-16 w-28 md:w-40 h-12 font-bold  md:text-xl lg:text-2xl text-l bg-[#64CCC5] text-[#053B50] ">
               Book Now
             </button>
           </a>
         </div>
       </div>
-      <div id="services" className="lg:h-60 md:h-[60vh] sm:h-[60vh] w-full bg-gradient-to-b flex lg:flex-row md:flex-col sm:flex-col justify-around items-center from-[#053B50] to-[#36dacf]">
+      {/* end sec2 */}
+
+      {/*  start sec3 */}
+      <div
+        id="services"
+        className="lg:h-60 md:h-[60vh] h-[60vh] w-full bg-gradient-to-b flex lg:flex-row md:flex-col sm:flex-col justify-around items-center from-[#053B50] to-[#36dacf]"
+      >
         <div className="relative">
-          <img className="w-64 md:w-80" src={icon1}></img>
-          <h1 className="absolute top-4 left-20 md:left-28 md:top-8 font-bold text-xl text-[#176B87] w-40 text-center">
+          <img className="lg:w-64 md:w-80 w-[39pc]" src={icon1}></img>
+          <h1 className="absolute top-2 text-sm text-center left-[40px] md:left-28 md:top-8 font-bold md:text-xl lg:text-xl lg:top-4 lg:left-20 lg:text-center text-[#176B87] ">
             Book Tickets Online
           </h1>
         </div>
         <div className="relative">
-          <img className="w-64 md:w-80" src={icon2}></img>
-          <h1 className="absolute top-4 left-20 md:left-24 md:top-8 font-bold text-xl text-[#176B87] w-46 text-center">
+          <img className="lg:w-64 md:w-80 w-[39pc]" src={icon2}></img>
+          <h1 className="absolute top-2 text-sm left-10 md:left-28 md:top-8 font-bold md:text-xl lg:text-xl lg:top-4 lg:left-20 text-center text-[#176B87] ">
             Pick The Nearest Station
           </h1>
         </div>
         <div className="relative">
-          <img  className="w-64 md:w-80"src={icon3}></img>
-          <h1 className="absolute top-4 left-24 md:left-32 md:top-8 font-bold text-xl text-[#176B87] w-32 text-center">
+          <img className="lg:w-64 md:w-80 w-[39pc]" src={icon3}></img>
+          <h1 className="absolute top-2 text-sm left-9 md:left-28 md:top-8 font-bold md:text-xl lg:text-xl lg:top-4 lg:left-20 text-center text-[#176B87] ">
             Schedule Your Trips
           </h1>
         </div>
       </div>
-      <div id="stations" className="h-screen md:h-[160vh] sm:h-[160vh] w-full flex flex-col justify-center items-center bg-gradient-to-b to-[#053B50] from-[#36dacf]">
-        <h1 className="font-bold lg:text-4xl md:text-xl sm:text-lg sm:w-96 md:w-96 lg:w-full md:mt-14 text-center text-[#EEEEEE] lg:mb-20 md:mb-10 sm:mb-10">
+      {/* <div
+        id="stations"
+        className="h-screen md:h-[160vh] w-full flex flex-col justify-center items-center bg-gradient-to-b to-[#053B50] from-[#36dacf]"
+      >
+        <h1 className="font-bold text-lg lg:text-4xl md:text-xl lg:mb-20 md:mb-10 lg:w-full m-6 md:mt-14 text-center text-[#EEEEEE] ">
           King Abdulaziz Project for Riyadh Public Transport Station
         </h1>
-        <div className="flex lg:flex-row md:mb-5 md:flex-col sm:mb-5 sm:flex-col md:flex-nowrap lg:flex-wrap lg:justify-center md:justify-between sm:justify-between lg:gap-x-56 lg:w-9/12 md:w-6/12 sm:w-6/12 sm:h-4/5 lg:h-4/5 md:h-4/5">
+        <div className="flex h-2/4 m-6 lg:flex-row md:mb-5 md:flex-col flex-col lg:flex-wrap lg:justify-center md:justify-between sm:justify-between lg:gap-x-56 lg:w-9/12 md:w-6/12 lg:h-4/5 md:h-4/5">
           <Card src={olaya} title="Olaya Station"></Card>
           <Card src={kafd} title="KAFD Metro Station"></Card>
           <Card src={western} title="Western Station"></Card>
           <Card src={hokm} title="Qasr Ak-Hokm Station"></Card>
         </div>
-      </div>
-      <div id="about" className="flex flex-col justify-center items-center bg-gradient-to-b from-[#053B50] to-[#36dacf] w-full h-80">
+
+        {/* </div> */}
+      {/* </div>  */}
+      {/* <div
+        id="about"
+        className="flex flex-col justify-center items-center bg-gradient-to-b from-[#053B50] to-[#36dacf] w-full h-80"
+      >
         <h1 className="font-bold text-4xl mb-5  text-[#EEEEEE]">About</h1>
         <h1 className="font-bold lg:text-xl md:text-sm sm:text-sm text-[#EEEEEE] w-3/5 text-center">
           The Metro Project is the backbone of the public transport network in
@@ -100,7 +117,7 @@ export default function LandingPage() {
       </div>
 
       {/* footer */}
-      <Footer isLoggedIn={isLoggedIn}></Footer>
+      {/* <Footer isLoggedIn={isLoggedIn}></Footer> */}
     </div>
   );
 }
