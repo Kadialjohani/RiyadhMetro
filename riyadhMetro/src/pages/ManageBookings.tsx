2e3aaef490d02c873ea40d8e93fc589cabfe92cc
Majd <majdthani@gmail.com>
2023-09-14 02:21:14 +0300

    edit of text

--- riyadhMetro/src/pages/ManageBookings.tsx
+++ riyadhMetro/src/pages/ManageBookings.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 import axios from "axios";
-import html2canvas from 'html2canvas'
-import jsPDF from 'jspdf'
-import { useRef } from 'react';
-import NavBar from '../component/NavBar'
-import Footer from '../component/Footer'
-import Ticket from '../component/Ticket'
-import Swal from 'sweetalert2'
-import { useNavigate } from 'react-router-dom';
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+import { useRef } from "react";
+import NavBar from "../component/NavBar";
+import Footer from "../component/Footer";
+import Ticket from "../component/Ticket";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 interface info {
-  id: string
-  from: string,
-  to: string,
-  date: string,
-  price: string,
+  id: string;
+  from: string;
+  to: string;
+  date: string;
+  price: string;
 }
 
 export default function ManageBookings() {
@@ -23,17 +23,17 @@ export default function ManageBookings() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   // for the pdf
-  const pdfRef = useRef<HTMLDivElement>(null)
+  const pdfRef = useRef<HTMLDivElement>(null);
 
   // get
   const [list, setList] = React.useState<info[]>([]);
 
-  const nav = useNavigate()
-  if(localStorage.getItem("isLogin") === "true"){
-    nav("/bookings")
-} else {
-  nav("/login")
-}
+  const nav = useNavigate();
+  if (localStorage.getItem("isLogin") === "true") {
+    nav("/bookings");
+  } else {
+    nav("/login");
+  }
 
   React.useEffect(() => {
     axios
@@ -45,32 +45,29 @@ export default function ManageBookings() {
   // del
   const deleteTicket = (id: string) => {
     Swal.fire({
-      title: 'Are you sure?',
+      title: "Are you sure?",
       text: "You won't be able to revert this!",
-      icon: 'warning',
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
         axios
-        .delete(`https://64fc603b605a026163ae6c99.mockapi.io/tickets/${id}`)
-        .then(() => {setList(
-          list.filter((item) => {
-            return item.id !== id;
-            
-          })
-        )}) 
-        Swal.fire(
-          'Deleted!',
-          'Your file has been deleted.',
-          'success'
-        )
+          .delete(`https://64fc603b605a026163ae6c99.mockapi.io/tickets/${id}`)
+          .then(() => {
+            setList(
+              list.filter((item) => {
+                return item.id !== id;
+              })
+            );
+          });
+        Swal.fire("Deleted!", "Your file has been deleted.", "success");
       }
-    })
+    });
   };
-// log in
+  // log in
   const handleLogin = () => {
     // Perform login logic here
     setIsLoggedIn(true);
@@ -84,49 +81,76 @@ export default function ManageBookings() {
   const handleLogout = () => {
     // Perform logout logic here
     setIsLoggedIn(false);
-    localStorage.setItem("isLogin", "false")
+    localStorage.setItem("isLogin", "false");
   };
   // download btn
   const download = () => {
     const input = pdfRef.current;
     if (input) {
-    html2canvas(input).then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
-      const pdf = new jsPDF('p', 'mm', 'a4', true);
-      const pdfWidth = pdf.internal.pageSize.getWidth();
-      const pdfHeight = pdf.internal.pageSize.getWidth();
-      const imgWidth = canvas.width;
-      const imgHeight = canvas.height;
-      const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
-      const imgX = (pdfWidth - imgWidth * ratio) / 2;
-      const imgY = 30;
-      pdf.addImage(imgData, 'PNG', imgX, imgY, imgWidth * ratio, imgHeight * ratio);
-      pdf.save('Metro-Ticket.pdf')
-    })}}
+      html2canvas(input).then((canvas) => {
+        const imgData = canvas.toDataURL("image/png");
+        const pdf = new jsPDF("p", "mm", "a4", true);
+        const pdfWidth = pdf.internal.pageSize.getWidth();
+        const pdfHeight = pdf.internal.pageSize.getWidth();
+        const imgWidth = canvas.width;
+        const imgHeight = canvas.height;
+        const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
+        const imgX = (pdfWidth - imgWidth * ratio) / 2;
+        const imgY = 30;
+        pdf.addImage(
+          imgData,
+          "PNG",
+          imgX,
+          imgY,
+          imgWidth * ratio,
+          imgHeight * ratio
+        );
+        pdf.save("Metro-Ticket.pdf");
+      });
+    }
+  };
 
   return (
     <div>
-      <div className='lg:h-[250vh] md:h-screen h-screen w-full flex flex-col bg-[#EEEEEE] relative'>
-      <NavBar isLoggedIn={!isLoggedIn} onLogin={handleLogin} onSignup={handleSignup} onLogout={handleLogout} ></NavBar>
-      <div ref={pdfRef} className='flex-col lg:w-4/5 md:w-11/12 w-full h-fit flex lg:p-5 md:p-2 p-1 rounded-xl bg-white justify-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-      <h1 className="text-[#176B87] flex w-fit lg:left-[40%] lg:top-[1%] lg:text-4xl md:left-[40%] md:top-[2%] md:text-2xl left-[32%] top-0 text-lg absolute">Your Tickets</h1>
+      <div className="lg:h-[250vh] md:h-screen h-screen w-full flex flex-col bg-[#EEEEEE] relative">
+        <NavBar
+          isLoggedIn={!isLoggedIn}
+          onLogin={handleLogin}
+          onSignup={handleSignup}
+          onLogout={handleLogout}
+        ></NavBar>
+        <h1 className="text-[#176B87] flex w-fit lg:left-[40%] lg:top-[15%] lg:text-5xl md:left-[37%] md:top-[11%] md:text-3xl left-[33%] top-[20%] text-xl absolute">
+          Your Ticket
+        </h1>
+        <div
+          ref={pdfRef}
+          className="flex-col lg:w-4/5 md:w-11/12 w-full h-fit flex lg:p-5 md:p-2 p-1 rounded-xl bg-white justify-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        >
+          {list.map((item) => {
+            return (
+              <div key={item.id}>
+                <Ticket
+                  from={item.from}
+                  to={item.to}
+                  date={item.date}
+                  price={item.price}
+                  deleteOnclick={() => deleteTicket(item.id)}
+                ></Ticket>
+              </div>
+            );
+          })}
 
-      {list.map((item) => {
-        return (
-          <div  key={item.id}>
-            <Ticket  from={item.from} to={item.to} date={item.date} price={item.price} deleteOnclick={() => deleteTicket(item.id)}></Ticket>
-          </div>
-        )
-        })} 
-      
-      
-      <button className='mb-2 lg:text-xl md:text-lg sm:text-sm text-[#EEEEEE] font-bold bg-[#176B87] lg:w-36 lg:h-14 w-28 h-10 rounded-full' onClick={download}>Download</button>
+          <button
+            className="mb-2 lg:text-xl md:text-lg text-sm text-[#EEEEEE] font-bold bg-[#176B87] lg:w-36 lg:h-14 w-28 h-10 rounded-full"
+            onClick={download}
+          >
+            Download
+          </button>
+        </div>
       </div>
-      
-      </div>
-      
+
       {/* footer */}
       <Footer isLoggedIn={!isLoggedIn}></Footer>
     </div>
-  )
+  );
 }
--- riyadhMetro/src/pages/Payment.tsx
+++ riyadhMetro/src/pages/Payment.tsx
@@ -4,30 +4,28 @@ import Ticket from "../component/Ticket";
 import NavBar from "../component/NavBar";
 import Footer from "../component/Footer";
 import { useState } from "react";
-import Swal from 'sweetalert2'
-
+import Swal from "sweetalert2";
 
 export default function Payment() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const nav = useNavigate();
   // block no logged in users
-  if(localStorage.getItem("isLogin") === "true"){
-    nav("/payment")
-} else {
-  nav("/login")
-}
-
-// prevent logging oout during payment process
-const handleLogout = () => {
-  nav("/payment")
-  Swal.fire({
-    icon: "error",
-    title: "Sorry...",
-    text: "You can't log out, you have uncompleted payment",
-  });
-  
+  if (localStorage.getItem("isLogin") === "true") {
+    nav("/payment");
+  } else {
+    nav("/login");
   }
+
+  // prevent logging oout during payment process
+  const handleLogout = () => {
+    nav("/payment");
+    Swal.fire({
+      icon: "error",
+      title: "Sorry...",
+      text: "You can't log out, you have uncompleted payment",
+    });
+  };
   const showAlert = () => {
     axios
       .post("https://64fc603b605a026163ae6c99.mockapi.io/tickets", {
@@ -36,14 +34,11 @@ const handleLogout = () => {
         date: localStorage.getItem("date"),
         price: localStorage.getItem("price"),
       })
-      .then((res) =>
-        console.log(res)
-      );
+      .then((res) => console.log(res));
   };
   //only used for the state for tha nav component
   const handleLogin = () => {
     setIsLoggedIn(true);
-    
   };
   const handleSignup = () => {
     setIsLoggedIn(true);
@@ -51,18 +46,36 @@ const handleLogout = () => {
 
   return (
     <div className="bg-[#EEEEEE] h-[120vh] w-screen flex flex-col items-center justify-between">
-      <NavBar isLoggedIn={!isLoggedIn} onLogin={handleLogin} onSignup={handleSignup} onLogout={handleLogout} ></NavBar>
-    <div className="lg:rounded-xl md:rounded-xl bg-white p-1 lg:w-3/4 md:w-11/12 w-full flex flex-col lg:p-5 md:p-5">
-      <Ticket from={localStorage.getItem("from")} to={localStorage.getItem("to")} date={localStorage.getItem("date")} price={localStorage.getItem("price")}></Ticket>
-    {/* {localStorage.getItem("from")} */}
-    <div>
-      
-      <a href="https://buy.stripe.com/test_bIYaHObp12fjeaI144"><button onClick={showAlert} className="lg:text-2xl md:text-xl text-sm text-[#EEEEEE] font-bold bg-[#176B87] lg:w-40 lg:h-10 md:w-40 md:h-10 w-20 h-6 mb-2 rounded-full">Pay</button></a>
-
-    </div>
-    </div>
-    {/* footer */}
-    <Footer isLoggedIn={!isLoggedIn}></Footer>
+      <NavBar
+        isLoggedIn={!isLoggedIn}
+        onLogin={handleLogin}
+        onSignup={handleSignup}
+        onLogout={handleLogout}
+      ></NavBar>
+      <h1 className="text-[#176B87] flex w-fit lg:left-[40%] lg:top-[15%] lg:text-5xl md:left-[35%] md:top-[30%] md:text-3xl left-[30%] top-[30%] text-xl absolute">
+        Ticket Payment
+      </h1>
+      <div className="lg:rounded-xl md:rounded-xl bg-white p-1 lg:w-3/4 md:w-11/12 w-full flex flex-col lg:p-5 md:p-5">
+        <Ticket
+          from={localStorage.getItem("from")}
+          to={localStorage.getItem("to")}
+          date={localStorage.getItem("date")}
+          price={localStorage.getItem("price")}
+        ></Ticket>
+        {/* {localStorage.getItem("from")} */}
+        <div>
+          <a href="https://buy.stripe.com/test_bIYaHObp12fjeaI144">
+            <button
+              onClick={showAlert}
+              className="lg:text-2xl md:text-xl text-sm text-[#EEEEEE] font-bold bg-[#176B87] lg:w-40 lg:h-12 md:w-40 md:h-10 w-20 h-6 mb-2 rounded-full"
+            >
+              Pay
+            </button>
+          </a>
+        </div>
+      </div>
+      {/* footer */}
+      <Footer isLoggedIn={!isLoggedIn}></Footer>
     </div>
   );
 }
